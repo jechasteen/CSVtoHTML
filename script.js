7f486e19aaede39069f356ce2cd4c4bfbcbf80e9
Jonathan Chasteen <jonathan.chasteen@live.com>
2017-12-26 21:30:22 -0500

    Version 0.2
    Giant list of variables converted to dictionary.
    Small amount of formatting and refactoring.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// For now this script file does not actually
-// consider the contents of the text boxes
+// dictionary of html tags
+var h = {
+  button_add: '<button type="button" class="btn btn-primary btn-xs">add</button>',
+  table_o: '<table>',
+  table_c: "</table>",
+  tr_o: "<tr>",
+  tr_c: "</tr>",
+  td_o: "<td>",
+  td_c: "</td>",
+  string: "",
+  headline_o: "<h2>File: ",
+  headline_c: "</h2>",
+  preview: '<h4>Preview</h4>',
+  preview_textarea_o: '<textarea rows="4" cols="80" id=\"id',
+  preview_textarea_c: '\"></textarea>',
+  preview_ids: ["id0","id1","id2","id3","id4","id5","id6","id7","id8","id9","id10",
+                "id11","id12","id13","id14","id15","id16","id17","id18","id19",
+                "id20","id21","id22","id23","id24","id25","id26","id27","id28","id29",
+                "id30","id31","id32","id33","id34","id35","id36","id37","id38","id39",
+                "id40","id41","id42","id43","id44","id45","id46","id47","id48","id49","id50",]
+}
 
 // container for data
 var contents = [];
@@ -39,57 +58,38 @@ function onChange(event){
 }
 // end load contents
 
-// constants for table elements
-var h_button_add = '<button type="button" class="btn btn-primary btn-xs">add</button>';
-var h_table_o = '<table>';
-var h_table_c = "</table>";
-var h_tr_o = "<tr>";
-var h_tr_c = "</tr>";
-var h_td_o = "<td>";
-var h_td_c = "</td>";
-var h_string = "";
-var h_headline_o ="<h2>File: ";
-var h_headline_c ="</h2>";
-var h_preview = '<h4>Preview</h4>';
-var h_preview_textarea_o = '<textarea rows="4" cols="80" id=\"id';
-var h_preview_textarea_c = '\"></textarea>';
-var h_preview_ids = ["id0","id1","id2","id3","id4","id5","id6","id7","id8","id9","id10",
-"id11","id12","id13","id14","id15","id16","id17","id18","id19",
-"id20","id21","id22","id23","id24","id25","id26","id27","id28","id29",
-"id30","id31","id32","id33","id34","id35","id36","id37","id38","id39",
-"id40","id41","id42","id43","id44","id45","id46","id47","id48","id49","id50",]
-/* var h_class = ' class=""';
-   var h_id = ' id=""'; */
-
+// do the actual parsing of each file's contents
 function parseCSV(csv){
   var output = "";  // string to hold result
   var linestart = true; //first run is a linestart.
   //open table
-  output += h_table_o;
+  output += h.table_o;
 
   //start to parse string
   for (var i = 0; i < csv.length; i++){
     // at linestart, add tr, td, and button then
     // set linestart to false and continue
     if (linestart === true){
-      output += h_tr_o + h_td_o + h_button_add + h_td_c + h_td_o;
+      output += h.tr_o + h.td_o + h.button_add + h.td_c + h.td_o;
       linestart = false;
     }
     if (csv[i] === "\n") {
       console.log("reached");
-      output += h_td_c + h_tr_c;
+      output += h.td_c + h.tr_c;
       linestart = true;
     } else if (csv[i] === ','){
-      output += h_td_c + h_td_o;
+      output += h.td_c + h.td_o;
     } else if (csv[i] !== ','){
       output += csv[i];
     } 
   }
-  output += h_table_c;
+  output += h.table_c;
 
   return output;
 }
+//end parse
 
+// Interface for converting one array ot the other: CSV -> HTML
 function convertAll(){
   var h_contents = [];
 
@@ -100,7 +100,9 @@ function convertAll(){
   });
   return h_contents;
 }
+// end array conversion
 
+// function for onclick
 function handleClick(){
   if (fs === undefined){
     alert("Choose at least one .csv file");
@@ -111,11 +113,12 @@ function handleClick(){
   var newlines_regexp = /(\r\n|\n|\r)/gm;
   for (var i = 0; i < h_result.length; i++){
     h_result[i] = h_result[i].replace(newlines_regexp, "");
-    h_total += h_headline_o + fs[i].name + h_headline_c + h_preview_textarea_o + i + h_preview_textarea_c + h_preview + h_result[i];
+    h_total += h.headline_o + fs[i].name + h.headline_c + h.preview_textarea_o + i + h.preview_textarea_c + h.preview + h_result[i];
   }
   
   document.getElementById("preview").innerHTML = h_total;
   for (var i = 0; i < h_result.length; i++){
-    document.getElementById(h_preview_ids[i]).value = h_result[i];
+    document.getElementById(h.preview_ids[i]).value = h_result[i];
   }
 }
+// end handleClick
